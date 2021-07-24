b4b2ff2a3e81f9bc08660624442b2c2ab7a4f3f7
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-07-24 16:56:28 +0900

    JavaScript　sum_exp up_data

--- app/models/out_put.rb
+++ app/models/out_put.rb
@@ -1,6 +1,6 @@
 class OutPut < ActiveHash::Base
   self.data = [
-    { id: 1, name:'----' },  { id: 2, name:'なし', multiple:1 }, {id: 3, name:'あり', multiple:1.5 }
+    { id: 1, name:'----' },  { id: 2, name:'なし', multiple:1 }, {id: 3, name:'あり', multiple:2 }
   ]
   include ActiveHash::Associations
   has_many :sum_exps
--- app/models/technique.rb
+++ app/models/technique.rb
@@ -1,6 +1,6 @@
 class Technique < ActiveHash::Base
   self.data = [
-    { id: 1, name:'----' }, { id: 2, name:'していない', multiple:1 }, { id: 3, name:'した', multiple:1.5 }
+    { id: 1, name:'----' }, { id: 2, name:'していない', multiple:1 }, { id: 3, name:'した', multiple:2 }
   ]
   include ActiveHash::Associations
   has_many :sum_exps
