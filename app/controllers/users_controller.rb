04d48957a5363c34988cf1a6e6979aacf80ab93d
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-08-05 18:29:29 +0900

    redirect_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:show, :edit]
 
   def new
   end
 
   def show
     @user = User.find(params[:id])
+    unless  @user.id == current_user.id
+      redirect_to root_path
+    end
     @skills = @user.skills
+
   end
 
   def edit
@@ -17,4 +21,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
   end
+
+ 
 end
