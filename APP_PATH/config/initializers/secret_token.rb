16fab1906675ecdd60bd981bb5a1b559e2811d07
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-08-10 12:45:48 +0900

    in_to_vendor

---
+++ APP_PATH/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+APPPATH::Application.config.secret_token = 'a3abe51796a16c69200e893d0587e6c34b0375b8b1ace26b2100f1ab1ec12b5d4592bf66d8481dc3f820130c187190b6ac6619b58201a6a8ffb98e777d980df3'
